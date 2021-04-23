c5b89b8ac3b416ce15e54682a44bad85b158a0fc
Rohit Pratapwar <pratapwarrohit13@live.com>
2021-04-23 23:07:47 +0530

    Add files via upload

---
+++ Table_Creation_addition.sql
@@ -0,0 +1,43 @@
+/* CREATE TABLE EMPLOYEEDEMOGRAPHICS
+-- CREATE TABLE EMPLOYEEDEMOGRAPHICS (
+--EMPLOYEID INT,
+--FIRSTNAME VARCHAR(50),
+--LASTNAME VARCHAR(50),
+--AGE INT,
+--GENDER VARCHAR(50)
+--)*/
+
+/* CREATE EMPLOYEESAKARY TABLE: 
+CREATE TABLE EMPLOYEESALARY
+(EMPLOYEEID INT,
+JOBTITLE VARCHAR(50),
+SALARY INT)
+*/
+
+INSERT INTO EMPLOYEEDEMOGRAPHICS VALUES
+(1001,'Jim','Halpert',30,'Male')
+
+INSERT INTO EMPLOYEEDEMOGRAPHICS VALUES(
+1002,'Pam','Beasely',30,'Female'),
+(1003,'Dwight','Schrute',29,'Male'),
+(1004,'Angela','Martin',31,'Female'),
+(1005,'Toby','Flenderson',32,'Male'),
+(1006,'Michael','Scott',35,'Male'),
+(1007,'Meredith','Palmer',32,'Female'),
+(1008,'Stanley','Hudson',38,'Male'),
+(1009,'Kevin','Malone',31,'Male'),
+(1010,'Rohit','Pratapwar',31,'Male')
+
+INSERT INTO EMPLOYEESALARY VALUES
+(1001,'SALESMAN',45000)
+
+INSERT INTO EMPLOYEESALARY VALUES
+(1002,'Receiptionist',36000),
+(1003,'Salesman',63000),
+(1004,'Accountant',47000),
+(1005,'HR',50000),
+(1006,'Regional Manager',65000),
+(1007,'Supplier Relations',41000),
+(1008,'Salesman',48000),
+(1009,'Accountant',42000),
+(1010,'Analyst',42000)
